00c1c4ad8d6e96c731e727315b0a9e200138fa58
SatyamBhushan <76408877+SatyamBhushan@users.noreply.github.com>
2021-06-08 00:22:32 +0530

    Add files via upload

---
+++ src/main/java/com/example/miniappdsign/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.miniappdsign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
